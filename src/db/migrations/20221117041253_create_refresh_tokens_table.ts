f1908fddabf858857400755379b446a88bcd7d54
Sachin Khadka <sachinworkk@gmail.com>
2022-12-12 11:22:48 +0545

    Refactor tables to text incase of large string data

--- src/db/migrations/20221117041253_create_refresh_tokens_table.ts
+++ src/db/migrations/20221117041253_create_refresh_tokens_table.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("refresh_tokens", (table) => {
     table.increments("id").primary().unsigned();
-    table.string("token", 1000).notNullable();
+    table.text("token").notNullable();
     table.integer("user_id").unsigned().notNullable();
     table.foreign("user_id").references("users.id").onDelete("CASCADE");
   });
