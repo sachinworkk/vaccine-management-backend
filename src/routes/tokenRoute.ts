ecfb6642ae0e60c4e65ace934b4e429c2aa95cbf
Sachin Khadka <sachinworkk@gmail.com>
2022-12-13 15:40:37 +0545

    Add swagger ui documentation for vaccine and token routes

--- src/routes/tokenRoute.ts
+++ src/routes/tokenRoute.ts
@@ -6,6 +6,36 @@ import * as routes from "../constants/urls";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /token/refresh:
+ *   post:
+ *     summary: Refresh access token.
+ *     tags:
+ *       - Token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *              refreshToken:
+ *                  type: string
+ *                  description: The user's refresh token.
+ *                  example: zxczxczxSDSDsdxczczxdasdDSAFDSA
+ *     responses:
+ *       200:
+ *         description: New Access token generated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                   example: asdaxczxfadsASDASDASDZXCZXCzxc
+ */
 router.post(routes.REFRESH_TOKEN, tokenController.refreshToken);
 
 export default router;
